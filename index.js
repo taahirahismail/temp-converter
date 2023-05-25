80016d86d37ea22c81a94d4af5013de6b51fd9d4
taahirahismail <taahirah.i@gmail.com>
2023-05-25 10:08:20 +0200

    I don't feel like doing this anymore lol

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function conv() {
-    
+function convC(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputF").innerHTML=((valNum*1.8)+32).toFixed(2);
+}
+
+function convF(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputC").innerHTML=((valNum-32)/1.8).toFixed(2);
+}
+
+function refresh() {
+  window.location.reload();
 }
